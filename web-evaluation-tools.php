e8fd14b1af433dfc97bff3827a2fa0af26beb9ac
Greg Kraus <gdkraus@ncsu.edu>
2014-03-26 12:05:18 -0400

    updated jQuery

--- web-evaluation-tools.php
+++ web-evaluation-tools.php
@@ -4,7 +4,7 @@ header("Content-type: text/javascript");
 
 //optionally load jQuery from a local source
 
-$jq = file_get_contents('jquery-1.9.1.min.js');
+$jq = file_get_contents('jquery-1.11.0.min.js');
 echo $jq;
 
 
